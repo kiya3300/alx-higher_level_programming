6342fe10e324066e86557e00c0b59e9a828b4a9d
kiya3300 <mekidesyitages.com>
2022-05-31 13:05:42 -0700

    please arfeh sera

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(-122, -96):
-    if i % 2 == 0:
-        let = chr(i * -1)
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
     else:
-        let = chr((i * -1) - 32)
-    print("{}".format(let), end="")
+        print('{:c}'.format(i - 32), end='')
