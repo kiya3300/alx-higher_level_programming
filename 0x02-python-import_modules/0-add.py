682714778bf9e3fd8cb5760d59859c64e1dbf087
Abel Yitages Demissie <99829666+kiya3300@users.noreply.github.com>
2022-06-02 06:53:35 -0700

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,15 +4,3 @@ if __name__ == "__main__":
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    #def add(a, b):
-
-   # My addition function
-
-       # Args:
-      #  a: first integer
-     #   b: second integer
-
-    #Returns:
-     #   The return value. a + b
-
-    #return (a + b)
