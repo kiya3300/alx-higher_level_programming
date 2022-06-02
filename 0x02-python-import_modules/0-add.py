19bd729c86d519e3f466a444cff4dffb24a90f8f
kiya3300 <mekidesyitages.com>
2022-06-02 06:40:35 -0700

    wilf uwf

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,7 @@ from add_0 import add
     a = 1
     b = 2
 print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    def add(a, b):
+    #def add(a, b):
 
     My addition function
 
@@ -13,6 +13,6 @@ print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         b: second integer
 
     Returns:
-        The return value. a + b
+     #   The return value. a + b
 
     return (a + b)
