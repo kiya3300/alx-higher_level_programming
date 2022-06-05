cfceb7e33f493d7713b9a166e4137850a12159a7
Abel Yitages Demissie <99829666+kiya3300@users.noreply.github.com>
2022-06-05 11:45:52 -0700

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for x in my_list:
         if idx == my_list.index(x):
             return(x)
-        elif idx > len(my_list):0
+        elif idx > len(my_list):
             return None
         elif idx < 0:
             return None
